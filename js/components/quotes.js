99911ca1777da5837216f0e14b1519d01c5b4dfc
zachary collins <zacharyjacobcollins@gmail.com>
2016-07-23 15:51:46 -0400

    updates

--- js/components/quotes.js
+++ js/components/quotes.js
@@ -1,20 +1,64 @@
 var quotes = new Vue({
-  el: '#quote-wall',
-  data: {
-    quotes: [
-      { quote: 'FooFooFooFooFooFooFooFooFooFooFooFoo' },
-      { quote: 'BarFooFooFooFooFooFooFooFooFooFooFooFooFoo' }, 
-      { quote: 'BarFooFooFooFooFooFooFooFooFooFooFooFooFoo' }, 
-    ]
-  }
+    el: '#vue-quote-card',
+    data: {
+        testQuote: {
+            text: 'Our hearts year for each other, we just want someone to finally see us. - C.A. Cardinal',
+            author: 'adfasdfsdafdsaf'
+        },
+        quotes: [{
+            text: 'Our hearts year for each other, we just want someone to finally see us. - C.A. Cardinal',
+            author: 'C.A. Cardinal'
+        }, {
+            text: 'The most intense fight a human will ever have is between the person they are and the person they are capable of becoming.',
+            author: 'Unknown'
+        }, {
+            text: 'When you look back on life you\'re going to regret the things you didn\'t do, you\'ll hardly ever regret the things that you did do.',
+            author: 'Collins'
+        }, {
+            text: 'Time is hawaiian tiki bars',
+            author: 'Houda Jarid'
+        }, {
+            text: 'Wake up early.  Stay up late.  When you finish you can take a nap',
+            author: 'Unknown'
+        }, {
+            text: 'Never push something.  Find what pulls.',
+            author: 'Collins'
+        }, {
+            text: 'The same fire that melts wax, sharpens steel.',
+            author: 'Unknown'
+        }, {
+            text: 'The best way to destroy an enemy is to make him a friend.',
+            author: 'Abraham Lincoln'
+        }, {
+            text: 'Never ruin a memory with the graspings of the morning.',
+            author: 'Collins'
+        }, {
+            text: 'Businessmen who do not know how to fight worry die young.',
+            author: 'Carnegie'
+        }, {
+            text: 'There are three things all wise men fear: the sea in storm, a night with no moon, and the anger of a gentle man.',
+            author: 'Patrick Rothfuss'
+        }, {
+            text: 'When you choose an action that is aligned with your personality, you strengthen it.  Likewise when you choose to do something that is out of character, you weaken your personality.',
+            author: 'Unknown'
+        }, {
+            text: 'Reflect.',
+            author: 'Unknown'
+        }, {
+            text: 'If you are wrong admit it quickly and emphatically.',
+            author: 'Carnegie'
+        }, {
+            text: '"All this time i was finding myself, but I, I didn\'t know that I was lost.',
+            author: 'Avicii'
+        }, {
+            text: 'Choose to see reality as reality is, not as you would like to see it.',
+            author: 'Collins'
+        }, {
+            text: 'What the eye doesn\'t see the heart doesnt grieve over.',
+            author: 'Unknown'
+        }, {
+            text: 'Details matter.  It\'s worth waiting to get it right',
+            author: 'Steve Jobs'
+        }]
+    }
 });
-
-var example1 = new Vue({
-  el: '#example-1',
-  data: {
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
-  }
-})
